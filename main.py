6c73d10cfe90cc2eaa8b929dbe9d93064fd4ee0c
Luo Yangxin <yasinlaw@outlook.com>
2023-12-11 16:58:28 +0800

    exp results with batch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import requests
 import torch.cuda
+from PIL import Image
 from transformers import (
     AutoImageProcessor,
     ViTModel,
@@ -6,8 +8,10 @@ from transformers import (
     ResNetForImageClassification,
     AutoFeatureExtractor,
 )
-from PIL import Image
-import requests
+
+batch_size = 512
+curr = 6
+
 
 model_list = [
     "google/vit-base-patch16-224-in21k",
@@ -18,29 +22,37 @@ model_list = [
     "microsoft/resnet-50",
     "microsoft/resnet-18",
 ]
-curr = 6
+
+if curr >= len(model_list):
+    exit(-1)
 
 torch.set_default_device("cuda")
 
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
+images = []
+
 processor = None
 model = None
 
-if not model_list[curr].find("resnet"):
+if "resnet" not in model_list[curr]:
     processor = ViTImageProcessor.from_pretrained(model_list[curr])
     model = ViTModel.from_pretrained(model_list[curr])
-elif model_list[curr].find("50"):
+elif "50" not in model_list[curr]:
     processor = AutoImageProcessor.from_pretrained(model_list[curr])
     model = ResNetForImageClassification.from_pretrained(model_list[curr])
-elif model_list[curr].find("18"):
+elif "18" not in model_list[curr]:
     processor = AutoFeatureExtractor.from_pretrained(model_list[curr])
     model = ResNetForImageClassification.from_pretrained(model_list[curr])
 
-inputs = processor(images=image, return_tensors="pt")
+for i in range(0, batch_size):
+    images.append(image)
+inputs = processor(images=images, return_tensors="pt")
 outputs = model(**inputs)
-
-with open(model_list[curr].replace("/", "-") + ".txt", "w") as f:
+torch.cuda.empty_cache()
+with open(
+    model_list[curr].replace("/", "-") + "-" + str(batch_size) + ".txt", "w"
+) as f:
     print(torch.cuda.memory_summary())
     f.write(model_list[curr] + "\n" + torch.cuda.memory_summary())
